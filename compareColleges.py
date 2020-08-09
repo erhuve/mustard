54b38eda4f3aee5585844e5acc442fc5c37a0e40
Anshul Ahluwalia <anshul.cgm@gmail.com>
2020-08-08 19:19:41 -0700

    Adding in pandas dataframe database logic

--- compareColleges.py
+++ compareColleges.py
@@ -1,4 +1,9 @@
 import argparse
+import pandas as pd
+import os
+from selenium import webdriver
+from scraper import read_data
+
 
 ap = argparse.ArgumentParser()
 
@@ -7,3 +12,42 @@ ap.add_argument("-c2", "--compare", help = "Second College to compare")
 ap.add_argument("-d", "--driver", help = "Path to Selenium Driver")
 
 args = vars(ap.parse_args())
+
+
+DRIVER_PATH = args["driver"]
+
+firstCollege = args["college"]
+secondCollege = args["compare"]
+
+dataFirst = {}
+dataSecond = {}
+initdfLength = 0
+if not os.path.exists("college_db.csv"):
+    print("Path doesn't exist")
+    dataFirst = read_data(firstCollege, DRIVER_PATH)
+    dataSecond = read_data(secondCollege, DRIVER_PATH)
+    df = pd.DataFrame([dataFirst, dataSecond])
+    #df = df.append(dataFirst, ignore_index = True)
+    #df = df.append(dataSecond, ignore_index = True)
+    df.to_csv('college_db.csv')
+    initdfLength = len(df)
+else:
+    df = pd.read_csv("college_db.csv")
+    initdfLength = len(df)
+    if len(df[df['name'] == firstCollege]) == 0:
+        print("Dataframe found but college not found")
+        dataFirst = read_data(firstCollege, DRIVER_PATH)
+        df = df.append(dataFirst, ignore_index = True)
+    else:
+        dataFirst = df[df["name"] == firstCollege].iloc[0].to_dict()
+    if len(df[df['name'] == secondCollege]) == 0:
+        dataSecond = read_data(secondCollege, DRIVER_PATH)
+        df = df.append(dataSecond, ignore_index = True)
+    else:
+        dataSecond = df[df["name"] == secondCollege].iloc[0].to_dict()
+
+#print(f"First college data is {dataFirst}")
+#print(f"Second college data is {dataSecond}")
+compare_colleges(dataFirst, dataSecond)
+if len(df) > initdfLength:
+    df.to_csv('college_db.csv')
