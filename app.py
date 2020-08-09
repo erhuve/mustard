91f9cbbe163ef151bd448c7d62a43344a54ca1c8
erhuve <rlhive@hotmail.com>
2020-08-08 17:38:41 -0700

    Added dropdown for colleges

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ from flask import request, render_template
 
 app = flask.Flask(__name__, template_folder='templates')
 DRIVER_PATH = '/Users/pastel/Downloads/chromedriverReal'
+with open('links.json') as json_file:
+    links= json.load(json_file)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
-        return(flask.render_template('main.html'))
+        colleges = links.keys()
+        return(flask.render_template('main.html', colleges1=colleges, colleges2=colleges))
 
     if flask.request.method ==  'POST':
-        college1 = request.form['college1']
-        college2 = request.form['college2']
+        college1 = request.form['colleges1']
+        college2 = request.form['colleges2']
         college1 = scraper.read_data(college1, DRIVER_PATH)
         college2 = scraper.read_data(college2, DRIVER_PATH)
         data = [college1, college2]
